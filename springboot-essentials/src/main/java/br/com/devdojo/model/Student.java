d48625dfa958000b21c43c65a3d45699cb7859ce
Vinícius Gomes Ferreira <neogedom@gmail.com>
2020-03-28 23:53:09 -0400

    04 - Setup do projeto com Spring Initializr

---
+++ springboot-essentials/src/main/java/br/com/devdojo/model/Student.java
@@ -0,0 +1,24 @@
+package br.com.devdojo.model;
+
+
+//A falta de getters e setters pode dar essa exceção
+// com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class
+// ou seja, um problema com o parse do JSON
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
